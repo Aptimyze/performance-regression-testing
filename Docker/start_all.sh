a7835b705de3dd03beb33b8a1dac2afa0c40131e
Thomas Zwickl <thomas.zwickl@gmail.com>
2017-11-01 23:00:16 +0100

    Update start script

--- Docker/start_all.sh
+++ Docker/start_all.sh
@@ -5,9 +5,9 @@ docker-compose build
 docker-compose up -d
 
 sleep 10s
-cp inspectit/businessContext.xml inspectit_cmr/ci/businessContext.xml
-cp inspectit/petclinic_base_services.xml /inspectit_cmr/ci/profiles/petclinic_base_services.xml
-cp inspectit/petclinic_remote.xml /inspectit_cmr/ci/profiles/petclinic_remote.xml
-cp inspectit/petclinic_sql_monitoring.xml /inspectit_cmr/ci/profiles/petclinic_sql_monitoring.xml
+sudo cp inspectit/businessContext.xml inspectit_cmr/ci/businessContext.xml
+sudo cp inspectit/petclinic_base_services.xml inspectit_cmr/ci/profiles/petclinic_base_services.xml
+sudo cp inspectit/petclinic_remote.xml inspectit_cmr/ci/profiles/petclinic_remote.xml
+sudo cp inspectit/petclinic_sql_monitoring.xml inspectit_cmr/ci/profiles/petclinic_sql_monitoring.xml
 
 docker restart inspectit_cmr
