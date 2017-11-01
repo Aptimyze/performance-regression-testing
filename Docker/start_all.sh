a699c7c8dfbece2e3b3ccb4ddae3e1e23ee02139
Thomas Zwickl <thomas.zwickl@gmail.com>
2017-11-01 21:53:22 +0100

    Update start script

--- Docker/start_all.sh
+++ Docker/start_all.sh
@@ -2,3 +2,11 @@
 
 sudo chown -R 1000:1000 jenkins
 docker-compose up
+
+sleep 10s
+cp inspectit/businessContext.xml inspectit_cmr/ci/businessContext.xml
+cp inspectit/petclinic_base_services.xml /inspectit_cmr/ci/profiles/petclinic_base_services.xml
+cp inspectit/petclinic_remote.xml /inspectit_cmr/ci/profiles/petclinic_remote.xml
+cp inspectit/petclinic_sql_monitoring.xml /inspectit_cmr/ci/profiles/petclinic_sql_monitoring.xml
+
+docker restart inspectit_cmr
