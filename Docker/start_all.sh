5f4ecbd60225ccd9ef7a5ddda028b571d329c36f
Thomas Zwickl <thomas.zwickl@gmail.com>
2017-11-01 23:25:34 +0100

    Update scripts.

--- Docker/start_all.sh
+++ Docker/start_all.sh
@@ -2,4 +2,9 @@
 
 sudo chown -R 1000:1000 jenkins
 docker-compose build
-docker-compose up
+docker-compose up -d
+
+sleep 10s
+sudo cp inspectit/businessContext.xml inspectit_cmr/ci/businessContext.xml
+sudo cp inspectit/default.xml inspectit_cmr/config/default.xml
+docker restart inspectit_cmr
