4a5334f2cd2a6380a00fc012e0e427101cc9409d
Thomas Zwickl <thomas.zwickl@gmail.com>
2017-11-01 23:07:40 +0100

    Update start script

--- Docker/start_all.sh
+++ Docker/start_all.sh
@@ -2,12 +2,4 @@
 
 sudo chown -R 1000:1000 jenkins
 docker-compose build
-docker-compose up -d
-
-sleep 10s
-sudo cp inspectit/businessContext.xml inspectit_cmr/ci/businessContext.xml
-sudo cp inspectit/petclinic_base_services.xml inspectit_cmr/ci/profiles/petclinic_base_services.xml
-sudo cp inspectit/petclinic_remote.xml inspectit_cmr/ci/profiles/petclinic_remote.xml
-sudo cp inspectit/petclinic_sql_monitoring.xml inspectit_cmr/ci/profiles/petclinic_sql_monitoring.xml
-
-docker restart inspectit_cmr
+docker-compose up
