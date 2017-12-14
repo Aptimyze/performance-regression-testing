a3902c3436eb86af88f68e21ba636d5731addcc7
Thomas Zwickl <thomas.zwickl@gmail.com>
2017-12-14 14:11:05 +0100

    Replace repository URLs with the ones from inspectit lab.

--- Docker/start_all.sh
+++ Docker/start_all.sh
@@ -9,3 +9,5 @@ sudo cp inspectit/default.xml inspectit_cmr/config/default.xml
 docker restart inspectit_cmr
 xdg-open http://localhost:3000
 xdg-open http://localhost:8090
+open http://localhost:3000
+open http://localhost:8090
